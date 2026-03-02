5dc7a401b6e502537dbac091d547f518f4dc0739
Guilherme <jg21504956@gmail.com>
2026-03-02 18:07:34 -0300

    initial commit

---
+++ src/tests/persistency.spec.ts
@@ -0,0 +1,10 @@
+describe("Realizando os primeiros testes com jest",()=>{
+    test('Primeiro Test' , ()=>{
+        const number =1 ;
+        expect(number).toBe(1);
+    })
+    it('Segundo Test' , ()=>{
+        const number =1 ;
+        expect(number).toBe(1);
+    })
+})
