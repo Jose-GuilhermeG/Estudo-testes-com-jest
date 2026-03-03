a02d08822cf1db29cc20af569cb990de947bb46d
Guilherme <jg21504956@gmail.com>
2026-03-03 14:59:38 -0300

    spyOn

--- src/tests/persistency.spec.ts
+++ src/tests/persistency.spec.ts
@@ -1,10 +1,28 @@
-describe("Realizando os primeiros testes com jest",()=>{
-    test('Primeiro Test' , ()=>{
-        const number =1 ;
-        expect(number).toBe(1);
+import { Persistency } from "../classes/persistency";
+
+describe("Testing Persistency",()=>{
+    afterEach(()=>{
+        jest.clearAllMocks();
     })
-    it('Segundo Test' , ()=>{
-        const number =1 ;
-        expect(number).toBe(1);
+
+    it("Should return undefined",()=>{
+        // system under test
+        const sut = new Persistency();
+        const returned = sut.saveOrder();
+        expect(returned).toBeUndefined();
+    })
+
+    it("Should call console.log once" , ()=>{
+        const sut = new Persistency();
+        const consoleSpy = jest.spyOn(console , 'log');
+        sut.saveOrder()
+        expect(consoleSpy).toHaveBeenCalledTimes(1);
+    })
+
+    it("Should call console.log with 'Pedido salvo com sucesso'",()=>{
+        const sut = new Persistency();
+        const consoleSpy = jest.spyOn(console , 'log');
+        sut.saveOrder();
+        expect(consoleSpy).toHaveBeenCalledWith("Pedido salvo com sucesso");
     })
 })
